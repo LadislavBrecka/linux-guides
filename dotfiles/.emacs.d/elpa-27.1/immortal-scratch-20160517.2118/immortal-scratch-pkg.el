3ffb8e5336c4e6bf30c543aa4fdd55bd8f60a55f
LadislavBrecka <lacoobrecka@gmail.com>
2023-02-22 21:32:13 +0100

    Move dotfiles to separated repository

--- dotfiles/.emacs.d/elpa-27.1/immortal-scratch-20160517.2118/immortal-scratch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from immortal-scratch-20160517.2118/immortal-scratch.el  -*- no-byte-compile: t -*-
-(define-package "immortal-scratch" "20160517.2118" "respawn the scratch buffer when it's killed" 'nil :commit "faeab0ad6c33c74c0cbd1dfcebffaa0690de40c6" :authors '(("Jonathan Kotta" . "jpkotta@gmail.com")) :maintainer '("Jonathan Kotta" . "jpkotta@gmail.com"))
--- dotfiles/.emacs.d/org-clock-save.el
+++
@@ -1,1 +0,0 @@
-;; org-clock-save.el - pop-os at <2023-01-26 Thu 18:37>
